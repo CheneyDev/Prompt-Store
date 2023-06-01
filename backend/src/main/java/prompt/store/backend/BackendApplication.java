ca827e510e7e7c4293c384968af0bb2f8ebbafef
CheneyDev <cheneydev@gmail.com>
2023-06-01 11:22:31 +0800

    Completed MySQL data source configuration.

--- backend/src/main/java/prompt/store/backend/BackendApplication.java
+++ backend/src/main/java/prompt/store/backend/BackendApplication.java
@@ -2,7 +2,9 @@ package prompt.store.backend;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.PropertySource;
 
+@PropertySource("classpath:credentials.properties")
 @SpringBootApplication
 public class BackendApplication {
 
