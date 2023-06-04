105a630eb25fbd7413d5163f9da4d8e6ca97d651
CheneyDev <cheneydev@gmail.com>
2023-06-04 16:46:51 +0800

    clear code

--- backend/src/main/java/prompt/store/backend/controller/AuthController.java
+++ backend/src/main/java/prompt/store/backend/controller/AuthController.java
@@ -1,18 +1,9 @@
 package prompt.store.backend.controller;
 
-import com.alibaba.fastjson.JSONObject;
-import jakarta.annotation.Resource;
-import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.security.authentication.AuthenticationManager;
-import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
-import org.springframework.security.core.Authentication;
-import org.springframework.security.core.context.SecurityContextHolder;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
+
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
-import prompt.store.backend.entity.RestBean;
-import prompt.store.backend.service.AuthorizeService;
+
 
 @RestController
 @RequestMapping("/api/auth")
