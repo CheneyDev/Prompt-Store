0104f62499e57091fa22e82cd9567a1e443e9399
CheneyDev <cheneydev@gmail.com>
2023-06-13 21:04:36 +0800

    some changes

--- backend/src/main/java/prompt/store/backend/utils/ObjectStorageUtil.java
+++ backend/src/main/java/prompt/store/backend/utils/ObjectStorageUtil.java
@@ -9,28 +9,31 @@ import com.amazonaws.services.s3.AmazonS3ClientBuilder;
 import com.amazonaws.services.s3.model.CannedAccessControlList;
 import com.amazonaws.services.s3.model.PutObjectRequest;
 import org.springframework.beans.factory.annotation.Value;
-import org.springframework.context.annotation.Bean;
 import org.springframework.stereotype.Component;
 
 import java.io.File;
-
 @Component
 public class ObjectStorageUtil {
 
     @Value("${cloudflare.r2.access.id}")
     public String accessId;
-    @Value("${cloudflare_r2_access_key}")
+
+    @Value("${cloudflare_r2.access.key}")
     public String secretKey;
+
+    @Value("${cloudflare.r2.endpoint}")
+    public String endpoint;
+
+    @Value("${cloudflare.r2.region}")
+    public String region;
 
     public AmazonS3 initS3Client() {
         AWSCredentials credentials = new BasicAWSCredentials(accessId, secretKey);
-        AmazonS3 s3Client = AmazonS3ClientBuilder.standard()
-                .withEndpointConfiguration(new AwsClientBuilder.EndpointConfiguration("https://7b3e05193016570b634981851d9bf159.r2.cloudflarestorage.com/", "us-east-1"))
+        return AmazonS3ClientBuilder.standard()
+                .withEndpointConfiguration(new AwsClientBuilder.EndpointConfiguration(endpoint, region))
                 .withCredentials(new AWSStaticCredentialsProvider(credentials))
                 .build();
-        return s3Client;
     }
-
     public void uploadFile(AmazonS3 s3Client,String bucketName, String fileName, File file) {
         s3Client.putObject(new PutObjectRequest(bucketName, fileName, file).withCannedAcl(CannedAccessControlList.PublicRead));
     }
